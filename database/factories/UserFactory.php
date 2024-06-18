d645a78cf5c0dcecbcdd50218f0661b0a82a6d5d
AngelPhenix <breathstylebro@gmail.com>
2024-06-18 03:00:37 +0200

    Added JobFactory for testing purposes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'admin' => false
         ];
     }
 
@@ -41,4 +43,15 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+
+    /**
+     * Indicate that the model is an admin.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'admin' => true,
+        ]);
+    } // App\Models\User::factory()->admin()->create()
 }
