1410603a0850a7660b8dc6d0b45ed2b5f39929af
AngelPhenix <breathstylebro@gmail.com>
2024-06-24 19:38:49 +0200

    Added delete button to edit form for jobs

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -41,10 +41,21 @@
 
   </div>
 
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <a href="/jobs/{{$job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 ml-5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+  <div class="mt-6 flex items-center justify-between gap-x-6">
+    <div class="flex items-center">
+        <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+    </div>
+
+    <div class="flex items-center gap-x-6">
+        <a href="/jobs/{{$job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 ml-5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+    </div>
+
   </div>
 </form>
 
+    <form method="post" action="/jobs/{{$job->id}}" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
 </x-layout>
