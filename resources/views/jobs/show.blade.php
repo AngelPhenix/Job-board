14eb6b07d4c234ee8d4819b388b23fc0f85de2f8
AngelPhenix <breathstylebro@gmail.com>
2024-06-24 19:07:49 +0200

    Added new button component (ahref) and placed one on the jobs.show page for edit purposes

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,4 +8,8 @@
         This job pays {{ $job['salary'] }} per year.
         This job has been posted by the employer named : {{ $job->employer->name }}
     </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/edit">Edit Job</x-button>
+    </p>
 </x-layout>
