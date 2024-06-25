5be9a3d82f17ed436ea848aeb49ccd446bf41e8d
AngelPhenix <breathstylebro@gmail.com>
2024-06-25 20:42:22 +0200

    Added a new form-error component

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -13,15 +13,13 @@
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
-          <x-form-label>$slot</x-form-label>
+          <x-form-label for="title">Title</x-form-label>
           <div class="mt-2">
             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
               <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Developer" required>
             </div>
 
-            @error('title')
-              <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }} </p>
-            @enderror
+            <x-form-error name="title"/>
           </div>
         </div>
 
