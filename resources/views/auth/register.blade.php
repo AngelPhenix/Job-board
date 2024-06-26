aa1216276894f671ccb5f3951e2c39014daa87a2
AngelPhenix <breathstylebro@gmail.com>
2024-06-27 01:51:12 +0200

    Added view auth.register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        Create a job
+        Register a new account
     </x-slot:heading>
 
 <form method="POST" action="/jobs">
@@ -8,24 +8,46 @@
 
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">We simply need a handful of details</p>
 
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
         <x-form-field>
-          <x-form-label for="title">Title</x-form-label>
+          <x-form-label for="first_name">First Name</x-form-label>
           <div class="mt-2">
-            <x-form-input name="title" id="title" placeholder="CEO" required/>
-            <x-form-error name="title"/>
+            <x-form-input name="first_name" id="first_name" required/>
+            <x-form-error name="first_name"/>
           </div>
         </x-form-field>
 
         <x-form-field>
-          <x-form-label for="salary">Salary</x-form-label>
+          <x-form-label for="last_name">Last Name</x-form-label>
           <div class="mt-2">
-            <x-form-input name="salary" id="salary" placeholder="50.000€" required/>
-            <x-form-error name="salary"/>
+            <x-form-input name="last_name" id="last_name" required/>
+            <x-form-error name="last_name"/>
+          </div>
+        </x-form-field>
+
+        <x-form-field>
+          <x-form-label for="email">Email</x-form-label>
+          <div class="mt-2">
+            <x-form-input name="email" id="email" type="email" required/>
+            <x-form-error name="email"/>
+          </div>
+        </x-form-field>
+
+        <x-form-field>
+          <x-form-label for="password">Password</x-form-label>
+          <div class="mt-2">
+            <x-form-input name="password" id="password" type="password" placeholder="Your Password" required/>
+            <x-form-error name="password"/>
+          </div>
+        </x-form-field>
+
+        <x-form-field>
+          <x-form-label for="password_confirmation">Confirm Password</x-form-label>
+          <div class="mt-2">
+            <x-form-input name="password_confirmation" id="password_confirmation" type="password" placeholder="Confirm your password" required/>
+            <x-form-error name="password_confirmation"/>
           </div>
         </x-form-field>
 
@@ -35,8 +57,8 @@
   </div>
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <x-form-button>Save</x-form-button>
+    <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+    <x-form-button>Register</x-form-button>
   </div>
 </form>
 
