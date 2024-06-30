64673b373c1708910d9ac7bb687bd2a82e2ea643
AngelPhenix <breathstylebro@gmail.com>
2024-06-30 02:21:00 +0200

    Fixed routes for edit/update/destroy

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/jobs/create', [JobController::class, 'create']);
 Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 Route::get('/jobs/{job}', [JobController::class, 'show']);
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware(['auth', 'can:edit,job']);
-Route::patch('/jobs/{job}', [JobController::class, 'update'])->middleware(['auth', 'can:edit-job,job']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware(['auth', 'can:edit-job,job']);
+Route::patch('/jobs/{job}', [JobController::class, 'update'])->middleware(['auth', 'can:edit,job']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware(['auth', 'can:edit,job']);
 
 
 
