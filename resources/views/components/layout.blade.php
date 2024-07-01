230469460ed6e36fbc014001b356ad2a4c9104d3
AngelPhenix <breathstylebro@gmail.com>
2024-07-01 18:53:58 +0200

    Added tailwind without CDN into project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    @vite(['resources/css/app.css'])
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body class="h-full">
 
