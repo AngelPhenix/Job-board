a4654ad9b843e5cf3ff8dbcae68a14ff61b4bba0
AngelPhenix <breathstylebro@gmail.com>
2024-07-14 02:39:28 +0200

    Added new form for adding fish

--- resources/views/fisherman/fish_create.blade.php
+++ resources/views/fisherman/fish_create.blade.php
@@ -1,5 +1,40 @@
 <x-layout>
-    <form method="" action="">
+    <x-slot:heading>
+        Create a new fish
+    </x-slot:heading>
 
-    </form>
+<form method="POST" action="/fish">
+    @csrf
+
+  <div class="space-y-12">
+    <div class="border-b border-gray-900/10 pb-12">
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+        <x-form-field>
+          <x-form-label for="name">Name</x-form-label>
+          <div class="mt-2">
+            <x-form-input name="name" id="name" placeholder="Sardine" required/>
+            <x-form-error name="name"/>
+          </div>
+        </x-form-field>
+
+        <x-form-field>
+          <x-form-label for="level">Level</x-form-label>
+          <div class="mt-2">
+            <x-form-input name="level" id="level" placeholder="50" required/>
+            <x-form-error name="level"/>
+          </div>
+        </x-form-field>
+
+      </div>
+    </div>
+
+  </div>
+
+  <div class="mt-6 flex items-center justify-end gap-x-6">
+    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+    <x-form-button>Save</x-form-button>
+  </div>
+</form>
+
 </x-layout>
