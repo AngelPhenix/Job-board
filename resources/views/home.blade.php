4a20afba338066aaeba11828654365faabc4e34b
AngelPhenix <breathstylebro@gmail.com>
2024-07-16 00:18:49 +0200

    Added some informations on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
     <x-slot:heading>Home Page</x-slot:heading>
 
-    <h1> Hello from Home page ! </h1>
+    <h1 class="text-2xl"> Welcome to the home page ! </h1>
+    <p class="pt-10"><u>Here's some informations you can find in there :</u></p>
+    <div class="pl-5">
+        <p>This website contains informations about the Fisherman job in Final Fantasy XIV</p>
+        <p>You'll be able to </p>
+    </div>
 </x-layout>
