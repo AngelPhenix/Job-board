e344c893a5074fac5bc77df2531134a135935828
AngelPhenix <breathstylebro@gmail.com>
2024-07-16 00:29:53 +0200

    Added new view for the home page to present informations about the website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,9 @@
 <x-layout>
     <x-slot:heading>Home Page</x-slot:heading>
 
-    <h1 class="text-2xl"> Welcome to the home page ! </h1>
-    <p class="pt-10"><u>Here's some informations you can find in there :</u></p>
+    <p><u>Here's some informations you can find in there :</u></p>
     <div class="pl-5">
         <p>This website contains informations about the Fisherman job in Final Fantasy XIV</p>
-        <p>You'll be able to </p>
+        <p>For now, we currently have : <u class="text-green-500 font-bold">{{ $fishNum }} fish</u> and <u class="text-red-500 font-bold">{{ $spotNum }} spots</u> in the database.</p>
     </div>
 </x-layout>
